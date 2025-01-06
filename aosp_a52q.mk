ab64e1a33087f360ec75099fef1bd1d6f13ece7e
koko-07870 <ko282ko82@gmail.com>
2025-01-06 19:11:08 +0530

    a52q: explicitly build updater
    Signed-off-by: koko-07870 <ko282ko82@gmail.com>

--- aosp_a52q.mk
+++ aosp_a52q.mk
@@ -20,6 +20,7 @@ $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := true
+IS_OFFICIAL := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aosp_a52q
